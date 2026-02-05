eeca355df16f65d648526f764813cf121048471d
ertugrulferit <ef11@kent.ac.uk>
2026-02-05 09:30:53 +0000

    .

--- camel.py
+++ camel.py
@@ -1,1 +1,5 @@
-# Enter your code here
+def camel_to_snake(name: str) -> str:
+    return name.lower().replace(" ", "_")
+
+string = input("Enter a camelCase string: ")
+print(camel_to_snake(string))
